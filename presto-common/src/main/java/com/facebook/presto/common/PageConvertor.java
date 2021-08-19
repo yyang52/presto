5797be3ecbf7b78842993c5f6a613339bebcbc82
yyang52 <yang10.yang@intel.com>
2021-08-20 04:30:54 +0800

    refractor PageConvertor, support int, long, double type

---
+++ presto-common/src/main/java/com/facebook/presto/common/PageConvertor.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.common;
+
+import com.facebook.presto.common.block.Block;
+import com.facebook.presto.common.block.BlockBuilder;
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+import static com.facebook.presto.common.type.BigintType.BIGINT;
+import static com.facebook.presto.common.type.DoubleType.DOUBLE;
+import static com.facebook.presto.common.type.IntegerType.INTEGER;
+import static java.lang.Double.longBitsToDouble;
+
+public class PageConvertor
+{
+    private PageConvertor() {}
+    private static final Unsafe _UNSAFE;
+    private static final String INT_TYPE_ = "Int";
+    private static final String LONG_TYPE_ = "Long";
+    private static final String DOUBLE_TYPE_ = "Double";
+
+    static
+    {
+        sun.misc.Unsafe unsafe;
+        try {
+            Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            unsafe = (sun.misc.Unsafe) unsafeField.get(null);
+        }
+        catch (Throwable cause) {
+            unsafe = null;
+        }
+        _UNSAFE = unsafe;
+    }
+
+    // Currently, we didn't handle null arrays
+    public static long[] toOffHeap(Page page, String[] types)
+    {
+        int totalBlockCount = page.getChannelCount();
+        int positionCount = page.getPositionCount();
+
+        long[] dataBuffers = new long[totalBlockCount];
+
+        int[] sizeForEachBlock = new int[totalBlockCount];
+        for (int i = 0; i < totalBlockCount; i++) {
+            Block block = page.getBlock(i);
+            long bufferSize = block.getSizeInBytes();
+            dataBuffers[i] = _UNSAFE.allocateMemory(bufferSize);
+            if (types[i].equals(INT_TYPE_)) {
+                sizeForEachBlock[i] = 4;
+                for (int j = 0; j < positionCount; j++) {
+                    _UNSAFE.putInt(dataBuffers[i] + j * sizeForEachBlock[i], block.getInt(j));
+                }
+            }
+            else if (types[i].equals(LONG_TYPE_)) {
+                sizeForEachBlock[i] = 8;
+                for (int j = 0; j < positionCount; j++) {
+                    _UNSAFE.putLong(dataBuffers[i] + j * sizeForEachBlock[i], block.getLong(j));
+                }
+            }
+            else if (types[i].equals(DOUBLE_TYPE_)) {
+                sizeForEachBlock[i] = 8;
+                for (int j = 0; j < positionCount; j++) {
+                    _UNSAFE.putDouble(dataBuffers[i] + j * sizeForEachBlock[i], longBitsToDouble(block.getLong(j)));
+                }
+            }
+            else {
+                throw new UnsupportedOperationException("block type " + block.getClass().getName() + " not supported!!");
+            }
+        }
+
+        return dataBuffers;
+    }
+
+    public static void destroyOffHeapBuffer(long[] dataBuffers)
+    {
+        for (long dataBuffer : dataBuffers) {
+            _UNSAFE.freeMemory(dataBuffer);
+        }
+    }
+
+    // just for test.
+    public static Unsafe getUnsafe()
+    {
+        return _UNSAFE;
+    }
+
+    // TODO: how to represent types? string or others?
+    public static Page toPage(long[] dataBuffers, String[] types, int positionCount)
+    {
+        assert (dataBuffers.length == types.length);
+        int blockSize = dataBuffers.length;
+        Block[] blocks = new Block[blockSize];
+        for (int i = 0; i < blockSize; i++) {
+            if (types[i].equals(INT_TYPE_)) {
+                BlockBuilder blockBuilder = INTEGER.createBlockBuilder(null, positionCount);
+                for (int j = 0; j < positionCount; j++) {
+                    INTEGER.writeLong(blockBuilder, _UNSAFE.getInt(dataBuffers[i] + Integer.BYTES * j));
+                }
+                blocks[i] = blockBuilder.build();
+            }
+            else if (types[i].equals(LONG_TYPE_)) {
+                BlockBuilder blockBuilder = BIGINT.createBlockBuilder(null, positionCount);
+                for (int j = 0; j < positionCount; j++) {
+                    BIGINT.writeLong(blockBuilder, _UNSAFE.getLong(dataBuffers[i] + Long.BYTES * j));
+                }
+                blocks[i] = blockBuilder.build();
+            }
+            else if (types[i].equals(DOUBLE_TYPE_)) {
+                BlockBuilder blockBuilder = DOUBLE.createBlockBuilder(null, positionCount);
+                for (int j = 0; j < positionCount; j++) {
+                    DOUBLE.writeDouble(blockBuilder, _UNSAFE.getDouble(dataBuffers[i] + Double.BYTES * j));
+                }
+                blocks[i] = blockBuilder.build();
+            }
+            else {
+                throw new UnsupportedOperationException("unsupported type: " + types[i]);
+            }
+        }
+        return new Page(positionCount, blocks);
+    }
+}
---
+++ presto-common/src/test/java/com/facebook/presto/common/TestPageConvertor.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.common;
+
+import com.facebook.presto.common.block.Block;
+import com.facebook.presto.common.block.BlockBuilder;
+import org.testng.annotations.Test;
+
+import static com.facebook.presto.common.type.BigintType.BIGINT;
+import static com.facebook.presto.common.type.DoubleType.DOUBLE;
+import static com.facebook.presto.common.type.IntegerType.INTEGER;
+import static java.lang.Double.longBitsToDouble;
+import static org.testng.Assert.assertEquals;
+
+public class TestPageConvertor
+{
+    @Test
+    public void testLong()
+    {
+        int entries = 10;
+        String[] types = {"Long", "Long", "Long"};
+        BlockBuilder blockBuilder = BIGINT.createBlockBuilder(null, entries);
+        for (int i = 0; i < entries; i++) {
+            BIGINT.writeLong(blockBuilder, i);
+        }
+        Block block = blockBuilder.build();
+        Page page = new Page(block, block, block);
+        long[] ret = PageConvertor.toOffHeap(page, types);
+        assertEquals(ret.length, 3);
+        long dataBuffer0 = ret[0];
+        for (int i = 0; i < entries; i++) {
+            assertEquals(PageConvertor.getUnsafe().getLong(dataBuffer0 + i * Long.BYTES), (long) i);
+        }
+        PageConvertor.destroyOffHeapBuffer(ret);
+    }
+
+    @Test
+    public void testdOUBLE()
+    {
+        int entries = 10;
+        String[] types = {"Double", "Double", "Double"};
+        BlockBuilder blockBuilder = DOUBLE.createBlockBuilder(null, entries);
+        for (int i = 0; i < entries; i++) {
+            DOUBLE.writeDouble(blockBuilder, i);
+        }
+        Block block = blockBuilder.build();
+        Page page = new Page(block, block, block);
+        long[] ret = PageConvertor.toOffHeap(page, types);
+        assertEquals(ret.length, 3);
+        long dataBuffer0 = ret[0];
+        for (int i = 0; i < entries; i++) {
+            assertEquals(PageConvertor.getUnsafe().getDouble(dataBuffer0 + i * Double.BYTES), (double) i);
+        }
+        PageConvertor.destroyOffHeapBuffer(ret);
+    }
+
+    @Test
+    public void testInt()
+    {
+        int entries = 10;
+        String[] types = {"Int", "Int", "Int"};
+        BlockBuilder blockBuilder = INTEGER.createBlockBuilder(null, entries);
+        for (int i = 0; i < entries; i++) {
+            INTEGER.writeLong(blockBuilder, i);
+        }
+        Block block = blockBuilder.build();
+        Page page = new Page(block, block, block);
+        long[] ret = PageConvertor.toOffHeap(page, types);
+        assertEquals(ret.length, 3);
+        long dataBuffer0 = ret[0];
+        for (int i = 0; i < entries; i++) {
+            assertEquals(PageConvertor.getUnsafe().getInt(dataBuffer0 + i * Integer.BYTES), i);
+        }
+        PageConvertor.destroyOffHeapBuffer(ret);
+    }
+
+    @Test
+    public void testToPage()
+    {
+        int entries = 10;
+        String[] types = {"Int", "Long", "Double"};
+        long[] dataBuffers = new long[3];
+
+        // int
+        dataBuffers[0] = PageConvertor.getUnsafe().allocateMemory(entries * Integer.BYTES);
+        for (int i = 0; i < entries; i++) {
+            PageConvertor.getUnsafe().putInt(dataBuffers[0] + i * Integer.BYTES, i * 10);
+        }
+
+        // Long
+        dataBuffers[1] = PageConvertor.getUnsafe().allocateMemory(entries * Long.BYTES);
+        for (int i = 0; i < entries; i++) {
+            PageConvertor.getUnsafe().putLong(dataBuffers[1] + i * Long.BYTES, i * 2);
+        }
+
+        // Double
+        dataBuffers[2] = PageConvertor.getUnsafe().allocateMemory(entries * Double.BYTES);
+        for (int i = 0; i < entries; i++) {
+            PageConvertor.getUnsafe().putDouble(dataBuffers[2] + i * Double.BYTES, i * 2.5);
+        }
+
+        Page page = PageConvertor.toPage(dataBuffers, types, entries);
+
+        assertEquals(page.getChannelCount(), 3);
+        assertEquals(page.getPositionCount(), 10);
+        for (int i = 0; i < entries; i++) {
+            assertEquals(page.getBlock(0).getInt(i), i * 10);
+            assertEquals(page.getBlock(1).getLong(i), i * 2);
+            assertEquals(longBitsToDouble(page.getBlock(2).getLong(i)), i * 2.5);
+        }
+    }
+}
